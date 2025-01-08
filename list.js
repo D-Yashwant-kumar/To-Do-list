7ec67ff411d54b45edfb5f36db7b8de83c5a13a8
D Yashwant Kumar <dyashwantkumar08gmail.com>
2025-01-08 19:26:23 +0530

    added functionality to the task input

--- list.js
+++ list.js
@@ -1,5 +1,27 @@
 const userName=localStorage.getItem("data");
+const btn = document.querySelector("button");
+const input= document.querySelector(".add");
+const listicon= document.querySelector(".added-tasks ul");
 
 let updatedName=document.querySelector(".name p");
 updatedName.innerText=`Hey, ${userName}`;
+
+btn.addEventListener("click", ()=>{
+    if(input.value===""){
+        alert("Add Tasks");
+    }else{
+        const icons =["fa-solid fa-trash-can", "fa-solid fa-pen-to-square", "fa-regular fa-circle-check"];
+        
+        icons.forEach((iconClass)=>{
+            const icon= document.createElement("i");
+            icon.className= iconClass;
+            const li =document.createElement("li");
+            li.innerHTML= input.value;
+            listicon.appendChild(li);
+            listicon.appendChild(icon);
+
+        })
+       
+    }
+})
 
